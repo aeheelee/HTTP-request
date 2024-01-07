911a97550584bdecce4e1a1a4faf2915183f3efc
이애희/서치앤디스커버리개발팀 <ah.lee@musinsa.com>
2024-01-07 18:06:28 +0900

    feat: useFetch custom hook, react.lazy()와 suspense 작업

--- src/App.js
+++ src/App.js
@@ -1,58 +1,42 @@
+import { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Navbar from './component/Navbar';
-import CreateBlog from './blogComponent/CreateBlog';
-import BlogDetails from './blogComponent/BlogDetail';
-import NotFound from './component/NotFound';
 import Footer from './component/Footer';
-import { useEffect, useState } from 'react';
-
-/* react.lazy()와 suspense를 사용해 App 컴포넌트를 리팩토링 해보세요. */
+import useFetch from 'util/useFetch';
 
 function App() {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
   /* get 메소드를 통해 데이터를 받아오는 useEffect hook은 컴포넌트 내 여기저기 존재하고 있습니다. */
   /* 해당 hook은 반복이 되는 부분이 있으므로 어떻게 custom hook으로 만들 수 있을지 고민해 봅시다. */
   /* util 폴더 내에 존재하는 useFetch에 custom hook을 작성해 주세요. */
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('http://localhost:3001/blogs')
-      .then(res => {
-        if (!res.ok) {
-          throw Error('could not fetch the data for that resource');
-        } 
-        return res.json();
-      })
-      .then(data => {
-        setIsPending(false);
-        setBlogs(data);
-        setError(null);
-      })
-      .catch(err => {
-        setIsPending(false);
-        setError(err.message);
-      })
-    }, 1000);
-  }, [])
+  const [blogs, isPending, error] = useFetch(`http://localhost:3001/blogs`);
+
+  /* react.lazy()와 suspense를 사용해 App 컴포넌트를 리팩토링 해보세요. */
+  const CreateBlogComponent = lazy(() => import('./blogComponent/CreateBlog'));
+  const BlogDetailsComponent = lazy(() => import('./blogComponent/BlogDetail'));
+  const NotFoundComponent = lazy(() => import('./component/NotFound'));
 
   return (
     <BrowserRouter>
-      { error && <div>{ error }</div> }
-        <div className="app">
-          <Navbar />
-          <div className="content">
+      {error && <div>{error}</div>}
+      <div className="app">
+        <Navbar />
+        <div className="content">
+          <Suspense fallback={<div>Loading...</div>}>
             <Routes>
-              <Route exact path="/" element={<Home blogs={blogs} isPending={isPending} />} />
-              <Route path="/create" element={<CreateBlog />} />
-              <Route path="/blogs/:id" element={<BlogDetails />} />
-              <Route path="/blogs/:id" element={<NotFound />} />
+              <Route
+                exact
+                path="/"
+                element={<Home blogs={blogs} isPending={isPending} />}
+              />
+              <Route path="/create" element={<CreateBlogComponent />} />
+              <Route path="/blogs/:id" element={<BlogDetailsComponent />} />
+              <Route path="/blogs/:id" element={<NotFoundComponent />} />
             </Routes>
-          </div>
-          <Footer/>
+          </Suspense>
         </div>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
--- src/util/useFetch.js
+++ src/util/useFetch.js
@@ -2,11 +2,35 @@ import { useState, useEffect } from 'react';
 
 const useFetch = (url) => {
   /* useState를 이용하여 data, isPending, error를 정의하세요. */
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+  const endPoint = `${url}`;
 
   /* useFetch 안의 중심 로직을 작성해주세요. */
+  useEffect(() => {
+    setIsPending(true);
+    setTimeout(() => {
+      fetch(endPoint)
+        .then((res) => {
+          if (!res.ok) {
+            throw Error('could not fetch the data for that resource');
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setIsPending(false);
+          setBlogs(data);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 1000);
+  }, [endPoint]);
 
-  return /* return 문을 작성해주세요. */
-}
+  return [blogs, setBlogs, isPending, error]; /* return 문을 작성해주세요. */
+};
 
- 
 export default useFetch;
